8b9b72c2e6a897866c7bd1707bf3ec54857612df
hafiz98m <hafizm35@gmail.com>
2023-05-18 18:09:13 +0700

    video-02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState, useEffect, useMemo, useCallback, useRef } from "react";
+
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 function App() {
+  const gridRef = useRef();
+  const [rowData, setRowData] = useState([
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+    { make: "Ford", model: "focus", price: 40000 },
+    { make: "Toyota", model: "Calca", price: 30000 },
+    { make: "BMW", model: "4 series", price: 50000 },
+  ]);
+
+  const [columDefs, setColumnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true,
+  }));
+
+  const cellClickedListener = useCallback((e) => {
+    console.log("cellClicked", e);
+  });
+
+  // useEffect(() => {
+  //   fetch("https://www.ag-grid.com/example-assets/rowdata.js")
+  //     .then((result) => result.json())
+  //     .then((rowData) => setRowData(rowData));
+  // });
+
+  const pushMeClicked = useCallback((e) => {
+    gridRef.current.api.deselectAll();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 500 }}>
+      <button onClick={pushMeClicked}>Push Me</button>
+      <AgGridReact
+        ref={gridRef}
+        onCellClicked={cellClickedListener}
+        rowData={rowData}
+        columnDefs={columDefs}
+        defaultColDef={defaultColDef}
+        rowSelection="multiple"
+        animateRow={true}
+      />
     </div>
   );
 }
